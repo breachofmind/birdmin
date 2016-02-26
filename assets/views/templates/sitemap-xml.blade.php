74a49a90c884fe327aeb5a44ec1884842b557d1d
Mike Adamczyk <mike.adamczyk@brightstar.com>
2016-02-26 08:54:45 -0600

    Fix google parse error

--- assets/views/templates/sitemap-xml.blade.php
+++ assets/views/templates/sitemap-xml.blade.php
@@ -2,13 +2,10 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xmlns:brd="http://bom.us/sitemaps"
-        brd:urlCount="{{$objects->count()}}"
         xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 
     @foreach($objects as $object)
         <url>
-            <brd:title>{{trim($object['title'])}}</brd:title>
             <loc>{{trim($object['url'])}}</loc>
             <changefreq>{{$generator->frequency}}</changefreq>
             <priority>{{$generator->priority}}</priority>
